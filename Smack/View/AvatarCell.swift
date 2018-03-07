56675c41ec8cd8624ad83369dd477f2b3addcb72
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2018-03-07 16:10:29 +0000

    Added code to display selected avatar

--- Smack/View/AvatarCell.swift
+++ Smack/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImg: UIImageView!
@@ -16,6 +21,17 @@ class AvatarCell: UICollectionViewCell {
         super.awakeFromNib()
         setupView()
     }
+    
+    func configureCell(index: Int, type: AvatarType) {
+        if type == .dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+    }
+
     
     func setupView() {
         self.layer.backgroundColor = UIColor.lightGray.cgColor
