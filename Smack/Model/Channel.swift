a1108db411e276dab9a14aeab017c4290102f32c
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2018-03-12 15:57:04 +0000

    added code to fetch channel info

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Stephen Learmonth on 12/03/2018.
+//  Copyright © 2018 Devslopes. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
